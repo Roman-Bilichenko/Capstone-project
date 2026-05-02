b9f3e01c63a3374cb6f1c416de618994c50aad9c
Roman Bilichenko <romabilichenko1@gmail.com>
2026-05-02 22:16:42 +0300

    add netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   root: "./src/",
@@ -6,11 +7,25 @@ export default defineConfig({
   build: {
     outDir: "../dist",
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main:           resolve(__dirname, "src/index.html"),
+        cart:           resolve(__dirname, "src/html/cart.html"),
+        catalog:        resolve(__dirname, "src/html/catalog.html"),
+        about:          resolve(__dirname, "src/html/about.html"),
+        contact:        resolve(__dirname, "src/html/contact.html"),
+        productDetails: resolve(__dirname, "src/html/product-details.html"),
+      },
+    },
   },
   server: {
     port: 5173,
     strictPort: true,
     host: true,
     open: true,
+    fs: {
+      allow: ["..", "../dist"],
+    },
   },
+  publicDir: "assets",
 });
